fb2f648c719933c72c65707af5a8ac6ff76339a5
Joshua Lee <joshualee@iconfigure.org>
2016-12-29 16:51:35 -1000

    added to identifiers to to locate the response code and uri

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const net = require('net');
 
 
+let serverName;
+let date;
+
 let server = net.createServer( (socket) => {
     socket.setEncoding('utf8')
+
     socket.on('data', (chunk) => {
+      let request = chunk.split( ' ');
+      let responseMethod = request[0];
+      let uriResponse = request[1];
+
       console.log(chunk);
+      // console.log(chunk.split(" "));
+      // console.log(uriResponse);
     })
 
 })
@@ -12,4 +22,3 @@ let server = net.createServer( (socket) => {
 server.listen(8080, '0.0.0.0', () => {
   console.log('Welcome to the server', server.address());
 })
-
