33cba4cf28808458f92bac25c76742af87475481
Chaz-C <chazcastro@Chazs-MacBook-Pro.local>
2016-12-29 18:43:00 -1000

    moved switch into server

--- server.js
+++ server.js
@@ -2,30 +2,8 @@
 const net = require('net');
 const error = require('./gist/404.js');
 
-
 let serverName;
 let date;
-
-function response(method, uri) {
-  console.log('response works');
-  switch(uri) {
-    case '/':
-    case '/index.html':
-      process.stdout.write(`index or /`);
-      break;
-    case '/hydrogen.html':
-      break;
-    case '/helium.html':
-      break;
-    case '/404.html':
-      process.stdout.write(error.error);
-      break;
-    case '/css/styles.css':
-      break;
-    default:
-      break;
-  }
-}
 
 let server = net.createServer( (socket) => {
     socket.setEncoding('utf8');
@@ -36,9 +14,23 @@ let server = net.createServer( (socket) => {
       let uriResponse = request[1];
 
       console.log(chunk);
-      // console.log(chunk.split(" "));
-      // console.log(uriResponse);
-      response(responseMethod, uriResponse);
+      switch(uriResponse) {
+        case '/':
+        case '/index.html':
+          process.stdout.write(`index or /`);
+          break;
+        case '/hydrogen.html':
+          break;
+        case '/helium.html':
+          break;
+        case '/404.html':
+          socket.write(error.error);
+          break;
+        case '/css/styles.css':
+          break;
+        default:
+          break;
+      }
     });
 
 });
