3956e25d8155b39db8865836e8284a56b3cb05d4
Joshua Lee <joshualee@iconfigure.org>
2016-12-31 03:56:22 -1000

    added timestamp identifier and modifications to switch statment

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
 // jshint esversion: 6
 const net = require('net');
 const error = require('./gist/404.js');
-
-let serverName;
-let date;
+const helium = require('./gist/helium.js');
+const hydrogen = require('./gist/hydrogen.js');
 
 let server = net.createServer( (socket) => {
     socket.setEncoding('utf8');
 
     socket.on('data', (chunk) => {
       let request = chunk.split( ' ');
-      let responseMethod = request[0];
-      let uriResponse = request[1];
+      let responseMethod = request[0];//GET PST
+      let uriResponse = request[1];//
+      let host = request[3]
+      let timeStamp = new Date();
+      // console.log(chunk);
 
-      console.log(chunk);
       switch(uriResponse) {
         case '/':
         case '/index.html':
-          process.stdout.write(`index or /`);
+          process.stdout.write(`${error.headerHandler(responseMethod, timeStamp, host)}\n \n ${error.body}`);
           break;
         case '/hydrogen.html':
+          process.stdout.write(`${hydrogen.headerHandler(responseMethod, timeStamp, host)}\n \n ${hydrogen.body}`);
           break;
         case '/helium.html':
+          process.stdout.write(`${helium.headerHandler(responseMethod, timeStamp, host)}\n \n ${helium.body}`);
           break;
         case '/404.html':
           socket.write(error.error);
@@ -34,3 +37,7 @@ let server = net.createServer( (socket) => {
     });
 
 });
+
+server.listen(8080, '0.0.0.0', () => {
+  console.log('Welcome here')
+})
